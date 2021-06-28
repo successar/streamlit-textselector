f92bf33e8a78a637bd20581c30fb0c3e84600ca0
Sarthak Jain <sarjain@adobe.com>
2021-06-28 17:19:46 -0400

    Initial Commit

---
+++ build/lib/TextHighlighter/frontend/build/precache-manifest.4cf1b3289ba1151ad500f809b7f24e93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adcf8ed1fd325782f08033b283a3a1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6869fdfdb383d9539391",
+    "url": "./static/js/2.5793b5da.chunk.js"
+  },
+  {
+    "revision": "71aa9e5fe8bf6f2bc9396bc9828f6810",
+    "url": "./static/js/2.5793b5da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb277ec06df3b8d89fdb",
+    "url": "./static/js/main.ca4748c2.chunk.js"
+  },
+  {
+    "revision": "21e7af4e54daed16e4e1",
+    "url": "./static/js/runtime-main.b9978a02.js"
+  }
+]);
