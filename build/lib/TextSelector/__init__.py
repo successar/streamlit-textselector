9646a23fee941ed3855ad835183b21601f016fe0
Sarthak Jain <sarjain@adobe.com>
2021-08-02 18:55:54 -0400

    Initial Commit

---
+++ build/lib/TextSelector/__init__.py
@@ -0,0 +1,60 @@
+import os
+import streamlit.components.v1 as components
+
+# Create a _RELEASE constant. We'll set this to False while we're developing
+# the component, and True when we're ready to package and distribute it.
+# (This is, of course, optional - there are innumerable ways to manage your
+# release process.)
+_RELEASE = True
+
+# Declare a Streamlit component. `declare_component` returns a function
+# that is used to create instances of the component. We're naming this
+# function "_component_func", with an underscore prefix, because we don't want
+# to expose it directly to users. Instead, we will create a custom wrapper
+# function, below, that will serve as our component's public API.
+
+# It's worth noting that this call to `declare_component` is the
+# *only thing* you need to do to create the binding between Streamlit and
+# your component frontend. Everything else we do in this file is simply a
+# best practice.
+
+if not _RELEASE:
+    TextSelector = components.declare_component(
+        # We give the component a simple, descriptive name ("my_component"
+        # does not fit this bill, so please choose something better for your
+        # own component :)
+        "TextSelector",
+        # Pass `url` here to tell Streamlit that the component will be served
+        # by the local dev server that you run via `npm run start`.
+        # (This is useful while your component is in development.)
+        url="http://localhost:3001",
+    )
+else:
+    # When we're distributing a production version of the component, we'll
+    # replace the `url` param with `path`, and point it to to the component's
+    # build directory:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    TextSelector = components.declare_component("TextSelector", path=build_dir)
+
+
+# Add some test code to play with the component while it's in development.
+# During development, we can run this just as we would any other Streamlit
+# app: `$ streamlit run my_component/__init__.py`
+if not _RELEASE:
+    import streamlit as st
+
+    cmap = ["#FF0000", "#00FF00", "#0000FF"]
+
+    text = "Comparison of ranitidine and lansoprazole in short-term low-dose triple therapy for Helicobacter pylori infection."
+    text = text.split()
+
+    colors = [cmap[i%3] for i in range(len(text))]
+    # Create an instance of our component with a constant `name` arg, and
+    # print its output value.
+    output = TextSelector(
+        tokens=text,
+        colors=colors
+    )
+
+    st.write(text[output["selected"]])
---
+++ build/lib/TextSelector/frontend/build/precache-manifest.fb6b227890bd7a0eda995a329e3a3f3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d187fbf585738abd1647c5f7a690a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82909ff4d42f102d3fa0",
+    "url": "./static/js/2.cd021c8d.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.cd021c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240a8c934bc8a3365fd3",
+    "url": "./static/js/main.f311331f.chunk.js"
+  },
+  {
+    "revision": "e4e4b8f5d4b206addb8b",
+    "url": "./static/js/runtime-main.6b082060.js"
+  }
+]);
